1690c2468e0b63a7e86f4632cc845684cd7180d4
Esperanza211 <khadijazaabar05@gmail.com>
2024-04-25 23:36:50 +0100

    classes

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,9 +1,4 @@
 export class HolbertonClass {
-  /**
-   * Constructor for HolbertonClass
-   * @param {number} year - The year of the class
-   * @param {string} location - The location of the class
-   */
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -17,17 +12,10 @@ export class HolbertonClass {
     return this._location;
   }
 }
-
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-  /**
-   * Constructor for StudentHolberton
-   * @param {string} firstName - The first name of the student
-   * @param {string} lastName - The last name of the student
-   * @param {HolbertonClass} holbertonClass - The HolbertonClass instance associated with the student
-   */
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -46,7 +34,6 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
