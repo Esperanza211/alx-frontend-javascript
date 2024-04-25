872bbb317beaaae903b535b817f145998f28d47f
Esperanza211 <khadijazaabar05@gmail.com>
2024-04-25 23:31:15 +0100

    classes

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,25 +1,37 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = this.validateAmount(amount);
-    this._currency = this.validateCurrency(currency);
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of class Currency');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(newAmount) {
-    this._amount = this.validateAmount(newAmount);
-  }
-
   get currency() {
     return this._currency;
   }
 
-  set currency(newCurrency) {
-    this._currency = this.validateCurrency(newCurrency);
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of class Currency');
+    }
+    this._currency = currency;
   }
 
   displayFullPrice() {
@@ -27,23 +39,12 @@ export default class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
-      throw new TypeError('Amount and conversion rate must be numbers');
-    }
-    return amount * conversionRate;
-  }
-
-  validateAmount(amount) {
     if (typeof amount !== 'number') {
       throw new TypeError('Amount must be a number');
+    } else if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
     }
-    return amount;
-  }
 
-  validateCurrency(currency) {
-    if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of class Currency');
-    }
-    return currency;
+    return amount * conversionRate;
   }
 }
