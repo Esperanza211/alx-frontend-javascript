18a3af9880e3dc1d9bfa7f791de1449a727b88d1
Esperanza211 <khadijazaabar05@gmail.com>
2024-04-25 23:34:55 +0100

    classes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,5 +1,11 @@
 export default class Airport {
   constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+
     this._name = name;
     this._code = code;
   }
@@ -7,5 +13,4 @@ export default class Airport {
   get [Symbol.toStringTag]() {
     return `${this._code}`;
   }
-	
 }
